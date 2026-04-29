f30fe1e5055820e1da9d7838c4a5783102a4dc6d
alan2309 <alankritarya15@gmail.com>
2026-04-29 19:33:54 +0530

    fix

--- apps/backend/src/processes/processes.module.ts
+++ apps/backend/src/processes/processes.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CloudflareService } from '../common/cloudflare.service';
+import { BillingModule } from '../billing/billing.module';
 import { OrdersModule } from '../orders/orders.module';
 import { AdminProcessController } from './admin-process.controller';
 import { AdminProcessService } from './admin-process.service';
@@ -8,6 +9,6 @@ import { AdminProcessService } from './admin-process.service';
     controllers: [AdminProcessController],
     providers: [AdminProcessService, CloudflareService],
     exports: [AdminProcessService],
-    imports: [OrdersModule]
+    imports: [OrdersModule, BillingModule]
 })
 export class ProcessesModule { }
